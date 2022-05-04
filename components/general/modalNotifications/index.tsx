313ec19f9e94cddd7bf6ac4a521e61046c8696a0
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-04 08:37:22 +0200

    chore(ModalNotifications): code cleanup

--- components/general/modalNotifications/index.tsx
+++ components/general/modalNotifications/index.tsx
@@ -1,5 +1,5 @@
 import type { ReactFC } from "../../../utils/types";
-import { motion, useAnimation } from "framer-motion";
+import { motion, useAnimation, Variants } from "framer-motion";
 import { useEffect } from "react";
 
 interface Props {
@@ -8,6 +8,44 @@ interface Props {
 
 	enabled: boolean;
 }
+
+const DivVariants: Variants = {
+	initial: { top: -100, width: 50, borderRadius: 100 },
+	entry1: {
+		top: 50,
+		opacity: 1,
+		transition: {
+			duration: 0.5,
+			bounce: 0.4,
+			type: "spring"
+		}
+	},
+	entry2: {
+		width: "fit-content",
+		borderRadius: 15,
+		transition: {
+			delay: 0.2,
+			duration: 2,
+			ease: [0.6, 0, 0.17, 1]
+		}
+	},
+	exit1: {
+		top: -50,
+		opacity: 0,
+		transition: {
+			duration: 0.5
+		}
+	},
+	exit2: {
+		top: -100,
+		width: 50,
+		borderRadius: 100,
+		transition: {
+			delay: 0.5,
+			duration: 0.01
+		}
+	}
+};
 
 const ModalNotifications: ReactFC<Props> = ({ title, icon, enabled }) => {
 	const getIcon = () => {
@@ -20,37 +58,16 @@ const ModalNotifications: ReactFC<Props> = ({ title, icon, enabled }) => {
 	const divController = useAnimation();
 	useEffect(() => {
 		if (enabled) {
-			void divController.start({
-				top: 50,
-				opacity: 1,
-				transition: {
-					duration: 0.5,
-					bounce: 0.4,
-					type: "spring"
-				}
-			});
-			void divController.start({
-				width: "fit-content",
-				borderRadius: 15,
-				transition: {
-					delay: 0.2,
-					duration: 2,
-					ease: [0.6, 0, 0.17, 1]
-				}
-			});
+			void divController.start("entry1");
+			void divController.start("entry2");
 		} else {
-			void divController.start({
-				top: -50,
-				opacity: 0,
-				transition: {
-					duration: 0.5
-				}
-			});
+			void divController.start("exit1");
+			void divController.start("exit2");
 		}
 	}, [enabled]);
 
 	return (
-		<motion.div initial={{ top: -100, width: 50, borderRadius: 100 }} animate={divController} className="modal-notification">
+		<motion.div initial="initial" variants={DivVariants} animate={divController} className="modal-notification">
 			<motion.span
 				initial={{ opacity: 0, marginLeft: 12 }}
 				animate={{ opacity: 1, marginLeft: 7, transition: { delay: 0.3, duration: 0.1 } }}
