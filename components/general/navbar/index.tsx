2e01089d17e4c8239e06b3b827d149ec129eee23
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-04 21:27:34 +0200

    feat: add functional mobile dropdown with close onClick

--- components/general/navbar/index.tsx
+++ components/general/navbar/index.tsx
@@ -1,34 +1,79 @@
-import { motion } from "framer-motion";
-import React from "react";
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect, useState } from "react";
 import type { ReactFC } from "../../../utils/types";
 import Button from "../button";
 import MenuButton from "./MenuButton";
 import Navlink from "./navlink";
 
 const Navbar: ReactFC = () => {
+	const [enabled, setEnabled] = useState(false);
+	const mobileNavbarController = useAnimation();
+
+	useEffect(() => {
+		if (enabled) {
+			void mobileNavbarController.start({
+				height: "100vh",
+				transition: {
+					duration: 1,
+					ease: [0.6, 0, 0.17, 1]
+				}
+			});
+		} else {
+			void mobileNavbarController.start({
+				height: "0vh",
+				transition: {
+					duration: 1,
+					ease: [0.6, 0, 0.17, 1]
+				}
+			});
+		}
+
+		return () => mobileNavbarController.stop();
+	}, [enabled]);
+
+	const onClick = () => setEnabled(!enabled);
+	const closeDropdown = () => setEnabled(false);
+
 	return (
-		<motion.div className="navbar-container">
-			<div className="navbar-nav">
-				<div className="navbar-logo">
-					<img className="navbar-logo-icon" src="/logo.png" alt="logo" />
-					<h2 className="navbar-logo-title">Stereo</h2>
-				</div>
+		<>
+			{/* Created to block items from moving under navbar */}
+			<div className="fake-navbar" />
+			<div className="navbar-container-wrapper">
+				<motion.div className="navbar-container">
+					<div className="navbar-nav">
+						<div className="navbar-logo">
+							<img className="navbar-logo-icon" src="/logo.png" alt="logo" />
+							<h2 className="navbar-logo-title">Stereo</h2>
+						</div>
 
-				{/* Navbar for PC */}
-				<div className="navbar-buttons">
-					<Navlink title="Commands" path="/commands" />
-					<Navlink title="FAQ" path="/faq" />
-					<Navlink title="Status" path="/status" external />
-				</div>
-			</div>
-			<div className="navbar-buttons-bot">
-				<Navlink title="Invite" path="/invite" external />
-				<Button title="Login" style="main" type="link" path="/api/login" />
-			</div>
+						{/* Navbar for PC */}
+						<div className="navbar-buttons">
+							<Navlink title="Commands" path="/commands" />
+							<Navlink title="FAQ" path="/faq" />
+							<Navlink title="Status" path="/status" external />
+						</div>
+					</div>
+					<div className="navbar-buttons-bot">
+						<Navlink title="Invite" path="/invite" external />
+						<Button title="Login" style="main" type="link" path="/api/login" />
+					</div>
 
-			{/* Navbar for mobile */}
-			<MenuButton onClick={() => void 0} />
-		</motion.div>
+					{/* Navbar for mobile */}
+					<MenuButton onClick={onClick} />
+				</motion.div>
+				<motion.ul initial={{ height: 0 }} animate={mobileNavbarController} className={`navbar-mobile ${enabled ? "enabled" : ""}`}>
+					<li className="navbar-buttons-mobile">
+						<Navlink onClick={closeDropdown} title="Commands" path="/commands" />
+						<Navlink onClick={closeDropdown} title="FAQ" path="/faq" />
+						<Navlink onClick={closeDropdown} title="Status" path="/status" external />
+					</li>
+					<li className="navbar-buttons-bot-mobile">
+						<Button onClick={closeDropdown} title="Invite" style="secondary-nav" type="link" path="/invite" external />
+						<Button onClick={closeDropdown} title="Login" style="main" type="link" path="/api/login" />
+					</li>
+				</motion.ul>
+			</div>
+		</>
 	);
 };
 
---
+++ pages/invite.tsx
@@ -0,0 +1,63 @@
+import type { GetStaticPropsContext, NextPage } from "next";
+import { useState } from "react";
+import ConfirmModal from "../components/extensions/ConfirmModal";
+import { loadTranslations } from "../utils/i18n";
+// import MultipleChoiceModal from "../components/extensions/MultipleChoiceModal";
+
+export const getStaticProps = async ({ locale }: GetStaticPropsContext) => {
+	const translations = await loadTranslations(locale ?? "en", ["common"]);
+	return {
+		props: {
+			...translations
+		}
+	};
+};
+
+const Home: NextPage = () => {
+	const [modal, setModal] = useState(false);
+	// const [items, setItems] = useState([
+	// 	{ key: 1, name: "None", selected: false },
+	// 	{ key: 2, name: "Low", selected: true },
+	// 	{ key: 3, name: "Medium", selected: false },
+	// 	{ key: 4, name: "Hard", selected: false },
+	// 	{ key: 5, name: "Extreme", selected: false }
+	// ]);
+
+	// const onSelected = (name: string) => {
+	// 	const item = items.find((v) => v.name === name)!;
+	// 	item.selected = true;
+
+	// 	const newItems = items
+	// 		.filter((i) => i.name !== name)
+	// 		.map((i) => {
+	// 			i.selected = false;
+	// 			return i;
+	// 		});
+
+	// 	setItems([item, ...newItems].sort((a, b) => a.key - b.key));
+	// };
+
+	return (
+		<main>
+			<h1>Template</h1>
+			<button onClick={() => setModal(true)}>open modal</button>
+			{/* <MultipleChoiceModal
+				onClick={() => setModal(false)}
+				isOpen={modal}
+				title="Choose a bassboost level"
+				options={items}
+				onSelected={onSelected}
+				onSelectedAfter="close"
+			/> */}
+			<ConfirmModal
+				onClick={() => setModal(false)}
+				isOpen={modal}
+				title="Are you sure you want to delete this playlist?"
+				onConfirm={() => void 0}
+				onConfirmAfter="notification"
+			/>
+		</main>
+	);
+};
+
+export default Home;
