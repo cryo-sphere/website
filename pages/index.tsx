86af749ef83b78646ce4ee5193b0728a573b36a1
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-06 14:48:50 +0200

    feat(Landing): add small details section

--- pages/index.tsx
+++ pages/index.tsx
@@ -216,57 +216,130 @@ const Home: NextPage = () => {
 	const { t } = useTranslation();
 
 	return (
-		<Page>
-			<NoteSvg />
-			<div className="landing-top-section">
-				<div className="landing-top-left">
-					<h1 className="landing-top-title">Stereo</h1>
-					<p className="landing-top-desc">Free high quality music everywhere. For all.</p>
-					<div className="landing-top-buttons">
-						<Button title={capitalise(t("common:invite"))} style="main" type="link" path="/invite" external />
-						<Button title={capitalise(t("navigation:dashboard"))} style="secondary" type="link" path="/dashboard" />
+		<>
+			<Page>
+				<NoteSvg />
+				<div className="landing-top-section">
+					<div className="landing-top-left">
+						<h1 className="landing-top-title">Stereo</h1>
+						<p className="landing-top-desc">Free high quality music everywhere. For all.</p>
+						<div className="landing-top-buttons">
+							<Button title={capitalise(t("common:invite"))} style="main" type="link" path="/invite" external />
+							<Button title={capitalise(t("navigation:dashboard"))} style="secondary" type="link" path="/dashboard" />
+						</div>
+					</div>
+					<div className="landing-top-right">
+						<div className="jukebox-animation">
+							<object className="jukebox-animation-jukebox" type="image/svg+xml" data="/content/landing/jukebox.svg" />
+							<NotesAnimationSvg />
+						</div>
 					</div>
 				</div>
-				<div className="landing-top-right">
-					<div className="jukebox-animation">
-						<object className="jukebox-animation-jukebox" type="image/svg+xml" data="/content/landing/jukebox.svg" />
-						<NotesAnimationSvg />
-					</div>
-				</div>
-			</div>
 
-			<div className="landing-big-section-container">
-				<div className="landing-big-section">
-					<div className="landing-big-section-text">
-						<h1 className="landing-big-title">Never gonna give... karaoke up</h1>
-						<p className="landing-big-text">
-							Apply your favourite filters like Karaoke. Play with it all day long, they will never let you down.
-						</p>
+				<div className="landing-big-section-container">
+					<div className="landing-big-section">
+						<div className="landing-big-section-text">
+							<h1 className="landing-big-title">Never gonna give... karaoke up</h1>
+							<p className="landing-big-text">
+								Apply your favourite filters like Karaoke. Play with it all day long, they will never let you down.
+							</p>
+						</div>
+						<img id="one" className="landing-big-image" src="/content/landing/karaoke.svg" alt="karaoke image" />
 					</div>
-					<img id="one" className="landing-big-image" src="/content/landing/karaoke.svg" alt="karaoke image" />
-				</div>
-				<div className="landing-big-section">
-					<img id="two" className="landing-big-image" src="/content/landing/permissions.svg" alt="karaoke image" />
-					<div className="landing-big-section-text">
-						<h1 className="landing-big-title">Nobody can hurt me, no more</h1>
-						<p className="landing-big-text">
-							Turn of commands for certain users or roles with one click of a button on the dashboard. Now they can’t mess with the bot
-							anymore!
-						</p>
+					<div className="landing-big-section">
+						<img id="two" className="landing-big-image" src="/content/landing/permissions.svg" alt="karaoke image" />
+						<div className="landing-big-section-text">
+							<h1 className="landing-big-title">Nobody can hurt me, no more</h1>
+							<p className="landing-big-text">
+								Turn of commands for certain users or roles with one click of a button on the dashboard. Now they can’t mess with the
+								bot anymore!
+							</p>
+						</div>
+					</div>
+					<div className="landing-big-section">
+						<div className="landing-big-section-text">
+							<h1 className="landing-big-title">Around the world, around the world...</h1>
+							<p className="landing-big-text">
+								Stereo is used by hundreds of users across the globe and not everyone speaks English, that’s why Stereo is translated
+								in multiple different languages so everyone can experience the fun and high quality audio their way.
+							</p>
+						</div>
+						<img id="three" className="landing-big-image" src="/content/landing/languages.svg" alt="karaoke image" />
 					</div>
 				</div>
-				<div className="landing-big-section">
-					<div className="landing-big-section-text">
-						<h1 className="landing-big-title">Around the world, around the world...</h1>
-						<p className="landing-big-text">
-							Stereo is used by hundreds of users across the globe and not everyone speaks English, that’s why Stereo is translated in
-							multiple different languages so everyone can experience the fun and high quality audio their way.
-						</p>
+			</Page>
+			<Page className="small-section">
+				<div className="landing-small-section-container">
+					<div className="landing-small-title">
+						<h3 className="landing-small-title-text">Still not convinced?</h3>
+						<p className="landing-small-desc">Don’t worry, we aren’t done yet!</p>
 					</div>
-					<img id="three" className="landing-big-image" src="/content/landing/languages.svg" alt="karaoke image" />
+					<div className="landing-small-topics">
+						<div className="landing-small-topics-left">
+							<div className="landing-small-topic">
+								<div className="landing-small-topic-icon">
+									<i className="fa-solid fa-sack-dollar" />
+								</div>
+								<div className="landing-small-topic-text">
+									<h1>Money, which money?</h1>
+									<p>Stereo is and will always be 100% free with no hidden costs.</p>
+								</div>
+							</div>
+							<div className="landing-small-topic">
+								<div className="landing-small-topic-icon">
+									<i className="fa-solid fa-server" />
+								</div>
+								<div className="landing-small-topic-text">
+									<h1>Downtime? That doesn’t exist.</h1>
+									<p>You won’t miss a single note with 99.99% guaranteed uptime.</p>
+								</div>
+							</div>
+							<div className="landing-small-topic">
+								<div className="landing-small-topic-icon">
+									<i className="fa-solid fa-sliders" />
+								</div>
+								<div className="landing-small-topic-text">
+									<h1>Commands? So outdated.</h1>
+									<p>Yes, we have a webplayer too! Now you can do whatever you want from the web, no more switching to Discord.</p>
+								</div>
+							</div>
+						</div>
+						<div className="landing-small-topics-right">
+							<div className="landing-small-topic">
+								<div className="landing-small-topic-icon">
+									<i id="special" className="fa-solid fa-gears" />
+								</div>
+								<div className="landing-small-topic-text">
+									<h1>Who doesn’t like dashboards?</h1>
+									<p>Change all the settings to your likings with one click of a button on the dashboard.</p>
+								</div>
+							</div>
+							<div className="landing-small-topic">
+								<div className="landing-small-topic-icon">
+									<i className="fa-solid fa-radio" />
+								</div>
+								<div className="landing-small-topic-text">
+									<h1>ughh, I hate using YouTube. 😠</h1>
+									<p>
+										Don’t worry, we got you covered! We support SoundCloud playback too! Spotify and Deezer are supported as
+										search aswell!
+									</p>
+								</div>
+							</div>
+							<div className="landing-small-topic">
+								<div className="landing-small-topic-icon">
+									<i className="fa-solid fa-list" />
+								</div>
+								<div className="landing-small-topic-text">
+									<h1>Playlists, full of songs to listen to.</h1>
+									<p>Stereo has it’s own playlist feature which allows you to store playable audio links with ease.</p>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-		</Page>
+			</Page>
+		</>
 	);
 };
 
