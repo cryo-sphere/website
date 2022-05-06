6baafc25aa1e8008b3b46a3c720156401ec333d0
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-06 22:54:45 +0200

    fix: i18n can be null

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -1,7 +1,7 @@
 import type { I18n } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-export const loadTranslations = async (i18n: I18n, locale: string | undefined, ns: string[] = []) => {
-	if (process.env.NODE_ENV === "development") await i18n.reloadResources();
+export const loadTranslations = async (i18n: I18n | null, locale: string | undefined, ns: string[] = []) => {
+	if (process.env.NODE_ENV === "development") await i18n?.reloadResources();
 	return serverSideTranslations(locale ?? "en", [...ns, "navigation", "common"]);
 };
