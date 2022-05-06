9bd4ffd4578d04a1fb576d271a94e4d921284806
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-06 22:40:08 +0200

    feat: add translation reload for dev environment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { GetStaticPropsContext, NextPage } from "next";
-import { useTranslation } from "next-i18next";
+import { i18n, useTranslation } from "next-i18next";
 import { InviteSection, SmallSection, TopSection } from "../components/pages/landing";
 import { loadTranslations } from "../utils/i18n";
 
 export const getStaticProps = async ({ locale }: GetStaticPropsContext) => {
-	const translations = await loadTranslations(locale ?? "en");
+	const translations = await loadTranslations(i18n!, locale ?? "en", ["landing"]);
 	return {
 		props: {
 			...translations
