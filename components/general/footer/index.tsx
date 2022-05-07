301e6ae2dd6fa427f228e414ff4abdd5bdebc383
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-07 19:52:50 +0200

    feat(Footer): add center component w/ links

--- components/general/footer/index.tsx
+++ components/general/footer/index.tsx
@@ -1,5 +1,6 @@
 // import { useTranslation } from "next-i18next";
 import type { ReactFC } from "../../../utils/types";
+import Navlink from "../navbar/navlink";
 import Page from "../page";
 import FooterCreditsLink from "./FooterCreditsLink";
 
@@ -24,6 +25,46 @@ const Footer: ReactFC = () => {
 						<img src="/vercel.svg" alt="vercel" />
 					</FooterCreditsLink>
 				</div>
+			</div>
+			<div className="footer-center-container">
+				<ul className="footer-links-list">
+					<li id="brand" className="footer-links-title">
+						Stereo
+					</li>
+					<li className="footer-links-link">
+						<Navlink path="/discord" title="Support" external />
+					</li>
+					<li className="footer-links-link">
+						<Navlink path="/github" title="GitHub" external />
+					</li>
+					<li className="footer-links-link">
+						<Navlink path="/paypal" title="PayPal" external />
+					</li>
+				</ul>
+				<ul className="footer-links-list">
+					<li className="footer-links-title">Links</li>
+					<li className="footer-links-link">
+						<Navlink path="/commands" title="Commands" />
+					</li>
+					<li className="footer-links-link">
+						<Navlink path="/faq" title="FAQ" />
+					</li>
+					<li className="footer-links-link">
+						<Navlink path="/status" title="Status" external />
+					</li>
+				</ul>
+				<ul className="footer-links-list">
+					<li className="footer-links-title">Legal</li>
+					<li className="footer-links-link">
+						<Navlink path="/tos" title="Terms of Service" />
+					</li>
+					<li className="footer-links-link">
+						<Navlink path="/privacy-policy" title="Privacy Policy" />
+					</li>
+					<li className="footer-links-link">
+						<Navlink path="/credits" title="Credits" />
+					</li>
+				</ul>
 			</div>
 		</Page>
 	);
