fcbb46594d3a76fe91241eed9a018221a4784eb7
DaanGamesDG <daan@daangamesdg.xyz>
2022-05-09 21:42:41 +0200

    feat: move shareable components to SelectComponents file

---
+++ components/general/select/SelectComponents.tsx
@@ -0,0 +1,130 @@
+/*
+function test<bool extends boolean>() {
+	const Control: ReactFC<ControlProps<Option, bool>> = (props) => {
+		const className = `react-select-control ${getEventNames(false, props.isFocused)}`;
+	
+		return (
+			<components.Control className={className} {...props}>
+				{props.children}
+			</components.Control>
+		);
+	};
+}
+
+test<true>();
+*/
+
+import { useTranslation } from "next-i18next";
+import type { ReactNode } from "react";
+import { ControlProps, components, DropdownIndicatorProps, InputProps, MenuProps, MenuListProps, OptionProps, NoticeProps } from "react-select";
+import type { ReactFC } from "../../../utils/types";
+import type { Option } from "./SelectTypes";
+
+export const getEventNames = (isSelected: boolean, isFocused: boolean) => {
+	const names: string[] = [];
+
+	if (isSelected) names.push("selected");
+	if (isFocused) names.push("focused");
+
+	return names.join(" ");
+};
+
+export function getCustomComponents<Multi extends boolean>() {
+	const Control: ReactFC<ControlProps<Option, Multi>> = (props) => {
+		const className = `react-select-control ${getEventNames(false, props.isFocused)}`;
+
+		return (
+			<components.Control className={className} {...props}>
+				{props.children}
+			</components.Control>
+		);
+	};
+
+	const DropdownIndicator: ReactFC<DropdownIndicatorProps<Option, Multi>> = (props) => {
+		const className = `react-select-indicator`;
+
+		return (
+			<components.DropdownIndicator className={className} {...props}>
+				{props.children}
+			</components.DropdownIndicator>
+		);
+	};
+
+	const Input: ReactFC<InputProps<Option, Multi>> = (props) => {
+		return (
+			<components.Input className="react-select-input" {...props}>
+				{props.children}
+			</components.Input>
+		);
+	};
+
+	const Menu: ReactFC<MenuProps<Option, Multi>> = (props) => {
+		return (
+			<components.Menu className="react-select-menu" {...props}>
+				{props.children}
+			</components.Menu>
+		);
+	};
+
+	const MenuList: ReactFC<MenuListProps<Option, Multi>> = (props) => {
+		return (
+			<components.MenuList className="react-select-menulist" {...props}>
+				{props.children}
+			</components.MenuList>
+		);
+	};
+
+	const Option: ReactFC<OptionProps<Option, Multi>> = (props) => {
+		let extra: ReactNode;
+		let color = "#fff";
+		if (props.data.icon) {
+			switch (props.data.icon.type) {
+				case "image":
+					extra = <img className="react-select-icon" src={props.data.icon.image} alt="" />;
+					break;
+				case "colour":
+					color = props.data.icon.colour;
+					extra = <p className="react-select-colour" style={{ backgroundColor: color }} />;
+					break;
+				case "icon":
+					extra = <i id="react-select-icon-icon" className={props.data.icon.icon} />;
+					break;
+				default:
+					break;
+			}
+		}
+
+		const className = `react-select-option ${getEventNames(props.isSelected, props.isFocused)}`;
+
+		return (
+			<>
+				<components.Option className={className} {...props}>
+					{extra}
+					<p className="react-select-option-value" style={{ color }}>
+						{props.children}
+					</p>
+				</components.Option>
+			</>
+		);
+	};
+
+	const NoOptionsMessage: ReactFC<NoticeProps<Option, Multi>> = (props) => {
+		const { t } = useTranslation();
+
+		return (
+			<components.NoOptionsMessage className="react-select-notice" {...props}>
+				{t("common:dropdown.no_options")}
+			</components.NoOptionsMessage>
+		);
+	};
+
+	return {
+		Control,
+		DropdownIndicator,
+		Input,
+		Menu,
+		MenuList,
+		Option,
+		NoOptionsMessage
+	};
+}
--- components/general/select/SingleSelect.tsx
+++ components/general/select/SingleSelect.tsx
@@ -1,48 +1,8 @@
 import type { ReactFC } from "../../../utils/types";
-import Select, {
-	OptionProps,
-	MenuListProps,
-	MenuProps,
-	ControlProps,
-	SingleValueProps,
-	InputProps,
-	DropdownIndicatorProps,
-	components,
-	StylesConfig,
-	NoticeProps
-} from "react-select";
+import Select, { SingleValueProps, components, StylesConfig } from "react-select";
 import { ReactNode, useState } from "react";
-import { useTranslation } from "next-i18next";
 import type { Option as OptionProp, SingleProps } from "./SelectTypes";
-
-const getEventNames = (isSelected: boolean, isFocused: boolean) => {
-	const names: string[] = [];
-
-	if (isSelected) names.push("selected");
-	if (isFocused) names.push("focused");
-
-	return names.join(" ");
-};
-
-const Control: ReactFC<ControlProps<OptionProp, false>> = (props) => {
-	const className = `react-select-control ${getEventNames(false, props.isFocused)}`;
-
-	return (
-		<components.Control className={className} {...props}>
-			{props.children}
-		</components.Control>
-	);
-};
-
-const DropdownIndicator: ReactFC<DropdownIndicatorProps<OptionProp, false>> = (props) => {
-	const className = `react-select-indicator`;
-
-	return (
-		<components.DropdownIndicator className={className} {...props}>
-			{props.children}
-		</components.DropdownIndicator>
-	);
-};
+import { getCustomComponents } from "./SelectComponents";
 
 const SingleValue: ReactFC<SingleValueProps<OptionProp, false>> = (props) => {
 	let extra: ReactNode;
@@ -73,74 +33,6 @@ const SingleValue: ReactFC<SingleValueProps<OptionProp, false>> = (props) => {
 	);
 };
 
-const Input: ReactFC<InputProps<OptionProp, false>> = (props) => {
-	return (
-		<components.Input className="react-select-input" {...props}>
-			{props.children}
-		</components.Input>
-	);
-};
-
-const Menu: ReactFC<MenuProps<OptionProp, false>> = (props) => {
-	return (
-		<components.Menu className="react-select-menu" {...props}>
-			{props.children}
-		</components.Menu>
-	);
-};
-
-const MenuList: ReactFC<MenuListProps<OptionProp, false>> = (props) => {
-	return (
-		<components.MenuList className="react-select-menulist" {...props}>
-			{props.children}
-		</components.MenuList>
-	);
-};
-
-const Option: ReactFC<OptionProps<OptionProp, false>> = (props) => {
-	let extra: ReactNode;
-	let color = "#fff";
-	if (props.data.icon) {
-		switch (props.data.icon.type) {
-			case "image":
-				extra = <img className="react-select-icon" src={props.data.icon.image} alt="" />;
-				break;
-			case "colour":
-				color = props.data.icon.colour;
-				extra = <p className="react-select-colour" style={{ backgroundColor: color }} />;
-				break;
-			case "icon":
-				extra = <i id="react-select-icon-icon" className={props.data.icon.icon} />;
-				break;
-			default:
-				break;
-		}
-	}
-
-	const className = `react-select-option ${getEventNames(props.isSelected, props.isFocused)}`;
-
-	return (
-		<>
-			<components.Option className={className} {...props}>
-				{extra}
-				<p className="react-select-option-value" style={{ color }}>
-					{props.children}
-				</p>
-			</components.Option>
-		</>
-	);
-};
-
-const NoOptionsMessage: ReactFC<NoticeProps<OptionProp, false>> = (props) => {
-	const { t } = useTranslation();
-
-	return (
-		<components.NoOptionsMessage className="react-select-notice" {...props}>
-			{t("common:dropdown.no_options")}
-		</components.NoOptionsMessage>
-	);
-};
-
 export const SingleSelect: ReactFC<SingleProps> = (props) => {
 	const [menuOpen, setMenuOpen] = useState(false);
 
@@ -148,6 +40,8 @@ export const SingleSelect: ReactFC<SingleProps> = (props) => {
 		dropdownIndicator: (base) => ({ ...base, transform: `rotate(${menuOpen ? "180deg" : "0deg"})` }),
 		indicatorSeparator: (base) => ({ ...base, display: "none" })
 	};
+
+	const components = getCustomComponents<false>();
 	return (
 		<Select
 			{...props}
@@ -155,7 +49,7 @@ export const SingleSelect: ReactFC<SingleProps> = (props) => {
 			onMenuClose={() => setMenuOpen(false)}
 			onMenuOpen={() => setMenuOpen(true)}
 			styles={styles}
-			components={{ Option, MenuList, Menu, Control, SingleValue, Input, DropdownIndicator, NoOptionsMessage }}
+			components={{ ...components, SingleValue }}
 		/>
 	);
 };
